5ffc7bc266eaaec6322f44a64e5c774d954f490f
Paisan Saeloe <6310682809@cn102>
2023-03-05 14:29:34 +0700

    add view.py signin

--- trafficmonitoring/user/views.py
+++ trafficmonitoring/user/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html', {
+                'message': 'Invalid credentials.'
+            })
     return render(request, 'user/signin.html')
 
 def signup(request):
