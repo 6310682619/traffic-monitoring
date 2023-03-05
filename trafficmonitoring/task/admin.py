ff6327d4f4c826e338618b36a2bfd8e349699567
Paisan Saeloe <6310682809@cn102>
2023-03-05 14:06:52 +0700

    add models and add admin models

--- trafficmonitoring/task/admin.py
+++ trafficmonitoring/task/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from task.models import Task, Result, Input, Loop, Car
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("account", "date_time", "date_time_modify", "date_time_upload", "location", "description", "status")
+
+class InputAdmin(admin.ModelAdmin):
+    list_display = ("task", "video")
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ("input", "video", "weather")
+class LoopAdmin(admin.ModelAdmin):
+    list_display = ("input", "loop_name", "x", "y", "wigth", "height", "angle")
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("loop", "car_total", "car_type", "direction")
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Input, InputAdmin)
+admin.site.register(Result, ResultAdmin)
+admin.site.register(Loop, LoopAdmin)
+admin.site.register(Car, CarAdmin)
--- trafficmonitoring/task/models.py
+++ trafficmonitoring/task/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from user.models import Account
 # Create your models here.
+    
+class Task(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    date_time = models.CharField(max_length=100)
+    date_time_modify = models.CharField(max_length=100)
+    date_time_upload = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.CharField(max_length=256)
+    status = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.date_time}"
+    
+class Input(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    video = models.FileField(upload_to = 'uploads/', blank=True)
+
+    def __str__(self):
+        return f"{self.video}"
+    
+class Result(models.Model):
+    input = models.ForeignKey(Input, on_delete=models.CASCADE)
+    video = models.FileField(upload_to = 'uploads/', blank=True)
+    weather = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.weather}"
+
+class Loop(models.Model):
+    input = models.ForeignKey(Input, on_delete=models.CASCADE)
+    loop_name = models.CharField(max_length=100)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    wigth = models.IntegerField()
+    height = models.IntegerField()
+    angle = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.loop_name}"
+class Car(models.Model):
+    loop = models.ForeignKey(Loop, on_delete=models.CASCADE)
+    car_total = models.IntegerField(max_length=200)
+    car_type = models.CharField(max_length=20)
+    direction = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.car_total}"
+
+class total_Car(models.Model):
+    result = models.ForeignKey(Result, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20)
+    total = models.IntegerField(max_length=200)
+
+    def __str__(self):
+        return f"{self.type}"
